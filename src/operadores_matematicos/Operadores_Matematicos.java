49687c1b062bda2ade2b6528d8296810a0f54831
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-06-28 11:53:16 -0300

    build/ build.xml manifest.mf nbproject/ src/ test/

---
+++ src/operadores_matematicos/Operadores_Matematicos.java
@@ -0,0 +1,15 @@
+package operadores_matematicos;
+
+public class Operadores_Matematicos {
+
+    /* Método principal da classe */
+    public static void main(String[] args) {
+        double x = 7 + 3;
+        double y = 2 * 2;
+        double z = 10 - 1;
+        double a = 243 % 8;
+        System.out.println("Resultado: " + x);
+        System.out.println("Resultado: " + y);
+        System.out.println("Resultado: " + z);
+    }// Fim do método...
+}// Fim da classe...
